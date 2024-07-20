9d3003c426d4125a9b23eac10784e53f081b490f
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2024-07-21 00:11:32 +0300

    Add files

---
+++ main.py
@@ -0,0 +1,62 @@
+import uvicorn
+
+import logging
+import sys
+from os import getenv
+
+from fastapi import FastAPI, Request
+from handlers import my_router
+from routes import router
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.types import MenuButtonWebApp, WebAppInfo, Update
+from aiogram.client.session.aiohttp import AiohttpSession
+
+from contextlib import asynccontextmanager
+
+TOKEN = getenv('BOT_TOKEN')
+
+APP_BASE_URL = getenv("APP_BASE_URL")
+
+
+async def on_startup(bot: Bot, base_url: str):
+    await bot.set_webhook(f"{base_url}/webhook")
+    await bot.set_chat_menu_button(
+        menu_button=MenuButtonWebApp(text="Open Menu",
+                                     web_app=WebAppInfo(url=f"{base_url}/demo"))
+    )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await bot.set_webhook(url=f"{APP_BASE_URL}/webhook",  # {base_url}/webhook
+                          allowed_updates=dispatcher.resolve_used_update_types(),
+                          drop_pending_updates=True)
+    yield
+    await bot.delete_webhook()
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+session = AiohttpSession(proxy='http://proxy.server:3128')
+bot = Bot(token=TOKEN,
+          default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+          session=session)
+dispatcher = Dispatcher()
+dispatcher["base_url"] = APP_BASE_URL
+dispatcher.startup.register(on_startup)
+dispatcher.include_router(my_router)
+app = FastAPI(lifespan=lifespan)
+app.state.bot = bot
+app.include_router(router)
+
+
+@app.post("/webhook")
+async def webhook(request: Request) -> None:
+    update = Update.model_validate(await request.json(),
+                                   context={"bot": bot})
+    await dispatcher.feed_update(bot, update)
+
+
+uvicorn.run(app, host="0.0.0.0", port=8002)
---
+++ routes.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi import Form, Request, APIRouter
+from typing import Annotated
+
+
+from aiogram import Bot
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    WebAppInfo,
+)
+from aiogram.utils.web_app import (check_webapp_signature,
+                                   safe_parse_webapp_init_data)
+
+
+router = APIRouter()
+
+
+@router.get("/demo")
+async def demo_handler():
+    return FileResponse(Path(__file__).parent.resolve() / "demo.html")
+
+
+@router.post("/demo/checkData")
+async def check_data_handler(
+        auth: Annotated[str, Form(alias="_auth")],
+        request: Request):
+    bot: Bot = request.app.bot
+
+    if check_webapp_signature(bot.token, auth):
+        return {"ok": True}
+    return JSONResponse(content={"ok": False, "err": "Unauthorized"},
+                        status_code=401)
+
+
+@router.post("/demo/sendMessage")
+async def send_message_handler(auth: Annotated[str, Form(alias="_auth")],
+                               with_webview: Annotated[str, Form()],
+                               request: Request):
+    bot: Bot = request.app.bot
+    try:
+        web_app_init_data = safe_parse_webapp_init_data(token=bot.token,
+                                                        init_data=auth)
+    except ValueError:
+        return JSONResponse(content={"ok": False, "err": "Unauthorized"},
+                            status_code=401)
+
+    reply_markup = None
+    if with_webview == "1":
+        reply_markup = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text="Open",
+                        web_app=WebAppInfo(url=str(
+                            request.url.replace(scheme="https")
+                        )),
+                    )
+                ]
+            ]
+        )
+    await bot.answer_web_app_query(
+        web_app_query_id=web_app_init_data.query_id,
+        result=InlineQueryResultArticle(
+            id=web_app_init_data.query_id,
+            title="Demo",
+            input_message_content=InputTextMessageContent(
+                message_text="Hello, World!",
+                parse_mode=None,
+            ),
+            reply_markup=reply_markup,
+        ),
+    )
+    return {"ok": True}
